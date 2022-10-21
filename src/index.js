5dffad1b70e1dc44c4c088754ec250186e63ba4b
olamcastillo <Olamdiego@gmail.com>
2022-10-21 18:47:08 -0500

    1.12 - 1.14

--- src/index.js
+++ src/index.js
@@ -1,166 +1,72 @@
-import React, { useState } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, { useState } from 'react';
+import { createRoot } from 'react-dom/client';
 
-// const App = () => {
-//   const course = {
-//     name: 'Half Stack application development',
-//     parts: [
-//       {
-//         name: 'Fundamentals of React',
-//         exercises: 10
-//       },
-//       {
-//         name: 'Using props to pass data',
-//         exercises: 7
-//       },
-//       {
-//         name: 'State of a component',
-//         exercises: 14
-//       }
-//     ]
-//   }
-
-
-// 	const Header = (props) => {
-// 		return(
-
-// 				<h1>
-// 					{props.course.name}
-// 				</h1>
-// 		)
-// 	}
-
-// 	const Content = (props) => {
-// 		return(
-// 			<div>
-// 				<Part part={props.course.parts[0]} exercises={props.course.parts[0]}/>
-// 				<Part part={props.course.parts[1]} exercises={props.course.parts[1]}/>
-// 				<Part part={props.course.parts[2]} exercises={props.course.parts[2]}/>
-// 			</div>
-
-// 		)
-// 	}
-
-// 	const Part = (props) => {
-// 		return(
-// 			<p>
-// 					{props.part.name} {props.part.exercises}
-// 			</p>
-// 		)
-// 	}
-
-// 	const Total = (props) => {
-
-// 		return(
-// 			<p>
-// 					Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
-// 			</p>
-// 		)
-// 	}
-
-// 	return (
-//     <div>
-//       <Header course={course} />
-//       <Content course={course} />
-//       <Total course={course} />
-//     </div>
-//   )
-// }
-
-
-const Button = ( {handleGood, text}) => {
+const Button = ( {handleClick, text}) => {
 	return(
-		<button onClick={handleGood}>{text}</button>
+		<button onClick={handleClick}>{text}</button>
 	)
 }
 
-const Statistic = ( {text, value}) => {
-	return(
-				<tr>
-					<td>{text}</td>
-					<td>{value}</td>
-				</tr>
-	)
+const App = ( { anecdotes } ) => {
+
+	const [selected, setSelected] = useState(0)
+	const [random, setRandom] = useState(0)
+	const [points, setPoints] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 1  })
+
+	// take index of the item with more votes
+	const max = Object.entries(points).reduce(function(prev, curr){
+		return prev[1] > curr[1] ? prev : curr;
+	})[0];
+
+
+
+	// setting random
+	const randomNum = () => {
+		setRandom(Math.floor(Math.random() * anecdotes.length))
+	}
+
+	// call to set random num to set selected number 
+	const handleClick = () =>{ 
+		randomNum()
+		setSelected(random)
+	}
+
+	// create a copy of the poins object to avoid problems when changing the state directly
+	const votePost = () =>{ 
+	const copy = {...points};
+	copy[selected]+=1 
+	setPoints(copy)
 }
-
-const Statistics = ( {good, neutral, bad} ) => {
-	const all 		 = good + neutral + bad,
-				average  = (good-bad)/all,
-				positive = (good*100)/all;
-	return(
-		<table>
-			<tbody>
-      <Statistic text="good" value={good} />
-      <Statistic text="neutral" value={neutral}  />
-      <Statistic text="bad" value={bad}  />
-      <Statistic text="all" value={all}  />
-      <Statistic text="average" value={average}  />
-      <Statistic text="positive" value={positive + '%'}/>
-			</tbody>
-    </table>
-	)
-}
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-	const handleGood = () => {
-		setGood(good+1)
-	}
-	const handleNeutral = () => {
-		setNeutral(neutral+1)
-	}
-	const handleBad = () => {
-		setBad(bad+1)
-	}
-	if((good + neutral + bad) === 0) {
-		return(
-			<div>
-      <div>
-				<h2>
-					Give Feedback
-				</h2>
-				<Button handleGood={handleGood} text='good'/>
-				<Button handleGood={handleNeutral} text='neutral'/>
-				<Button handleGood={handleBad} text='bad'/>
-			</div>
-			<div>
-				<h2>
-					Statistics
-				</h2>
-				<p>No Feedback Given </p>
-				
-			</div>
-    </div>
-		)
-	}
 
   return (
     <div>
-      <div>
-				<h2>
-					Give Feedback
-				</h2>
-				<Button handleGood={handleGood} text='good'/>
-				<Button handleGood={handleNeutral} text='neutral'/>
-				<Button handleGood={handleBad} text='bad'/>
-			</div>
+      <h2>
+			{anecdotes[selected]}
+			</h2>
+			<p>{points[selected]} votes</p>
 			<div>
-				<h2>
-					Statistics
-				</h2>
-				<Statistics good={good} neutral={neutral} bad={bad} />
+				<Button handleClick={votePost} text='vote'/>
+				<Button handleClick={handleClick} text='next anecdote'/>
 			</div>
+			<h2>
+				Anecdote with most votes
+			</h2>
+			<p>
+				{anecdotes[max]}
+			</p>
     </div>
   )
 }
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />)
-
-
-// ReactDOM.render(<App />, document.getElementById('root'))
+root.render(<App anecdotes={anecdotes}/>)
