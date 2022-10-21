7d996c2f6313fd07151724c671f1aa4cdfb178a8
olamcastillo <Olamdiego@gmail.com>
2022-10-21 14:35:09 -0500

    1.6

--- src/index.js
+++ src/index.js
@@ -1,69 +1,121 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react'
+import { createRoot } from 'react-dom/client'
+
+// const App = () => {
+//   const course = {
+//     name: 'Half Stack application development',
+//     parts: [
+//       {
+//         name: 'Fundamentals of React',
+//         exercises: 10
+//       },
+//       {
+//         name: 'Using props to pass data',
+//         exercises: 7
+//       },
+//       {
+//         name: 'State of a component',
+//         exercises: 14
+//       }
+//     ]
+//   }
+
+
+// 	const Header = (props) => {
+// 		return(
+
+// 				<h1>
+// 					{props.course.name}
+// 				</h1>
+// 		)
+// 	}
+
+// 	const Content = (props) => {
+// 		return(
+// 			<div>
+// 				<Part part={props.course.parts[0]} exercises={props.course.parts[0]}/>
+// 				<Part part={props.course.parts[1]} exercises={props.course.parts[1]}/>
+// 				<Part part={props.course.parts[2]} exercises={props.course.parts[2]}/>
+// 			</div>
+
+// 		)
+// 	}
+
+// 	const Part = (props) => {
+// 		return(
+// 			<p>
+// 					{props.part.name} {props.part.exercises}
+// 			</p>
+// 		)
+// 	}
+
+// 	const Total = (props) => {
+
+// 		return(
+// 			<p>
+// 					Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
+// 			</p>
+// 		)
+// 	}
+
+// 	return (
+//     <div>
+//       <Header course={course} />
+//       <Content course={course} />
+//       <Total course={course} />
+//     </div>
+//   )
+// }
+
+
+const Button = ( {handleGood, text}) => {
+	return(
+		<button onClick={handleGood}>{text}</button>
+	)
+}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-
-	const Header = (props) => {
-		return(
-
-				<h1>
-					{props.course.name}
-				</h1>
-		)
+	const handleGood = () => {
+		setGood(good+1)
+	}
+	const handleNeutral = () => {
+		setNeutral(neutral+1)
+	}
+	const handleBad = () => {
+		setBad(bad+1)
 	}
 
-	const Content = (props) => {
-		return(
-			<div>
-				<Part part={props.course.parts[0]} exercises={props.course.parts[0]}/>
-				<Part part={props.course.parts[1]} exercises={props.course.parts[1]}/>
-				<Part part={props.course.parts[2]} exercises={props.course.parts[2]}/>
-			</div>
-
-		)
-	}
-
-	const Part = (props) => {
-		return(
-			<p>
-					{props.part.name} {props.part.exercises}
-			</p>
-		)
-	}
-
-	const Total = (props) => {
-
-		return(
-			<p>
-					Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
-			</p>
-		)
-	}
-
-	return (
+  return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <div>
+				<h2>
+					Give Feedback
+				</h2>
+				<Button handleGood={handleGood} text='good'/>
+				<Button handleGood={handleNeutral} text='neutral'/>
+				<Button handleGood={handleBad} text='bad'/>
+			</div>
+			<div>
+				<h2>
+					stadistics
+				</h2>
+				<p>good {good}</p>
+				<p>neutral {neutral}</p>
+				<p>bad {bad}</p>
+
+			</div>
     </div>
   )
 }
-ReactDOM.render(<App />, document.getElementById('root'))
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />)
+
+
+// ReactDOM.render(<App />, document.getElementById('root'))
